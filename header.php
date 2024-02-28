7ca0f7e350f5d232cf2fc0a1d496db3edeb871c6
spark <98943801+Sonar135@users.noreply.github.com>
2024-02-28 10:14:31 +0100

    first commit

---
+++ header.php
@@ -0,0 +1,21 @@
+<?php
+    include "connect.php";
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/heade.css?v=<?php echo ime();?>">
+    <script src="https://kit.fontawesome.com/b782cf5553.js" crossorigin="anonymous"></script>  
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+<script src="js/min.js"></script>
+<script src="js/dom.js"></script>
+</html>
