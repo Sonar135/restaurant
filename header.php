0342a8efa120936da465f12fe6a978df0eed9172
spark <98943801+Sonar135@users.noreply.github.com>
2024-02-29 15:46:52 +0100

    Add files via upload
    new commit

--- header.php
+++ header.php
@@ -7,14 +7,43 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="css/heade.css?v=<?php echo ime();?>">
+    <link rel="stylesheet" href="css/header.css?v=<?php echo time();?>">
     <script src="https://kit.fontawesome.com/b782cf5553.js" crossorigin="anonymous"></script>  
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    
+<div class="nav">
+            <div class="cent">
+                <div class="logo">
+
+                </div>
+
+                
+
+                <div class="menu">
+                    <div class="contact">
+                        <div class="icon">
+
+                        </div>
+
+                        <div class="text">
+                            <h4>267 Park Avenue</h4>
+                            <h4>New York, NY 90210</h4>
+                        </div>
+                    </div>
+
+                    <div class="contact">
+                        
+                        </div>
+
+                        <div class="contact">
+                        
+                        </div>
+                </div>
+            </div>
+        </div>
 </body>
 <script src="js/min.js"></script>
 <script src="js/dom.js"></script>
