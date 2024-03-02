72cc8aefa7c5e3ceb60c18aee0afa596ce16f250
spark <98943801+Sonar135@users.noreply.github.com>
2024-03-03 00:31:00 +0100

    Add files via upload

--- restaurant.sql
+++ restaurant.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 02, 2024 at 02:26 PM
+-- Generation Time: Mar 02, 2024 at 11:03 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.1.25
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -52,8 +52,23 @@ CREATE TABLE `item` (
   `quantity` int(255) DEFAULT NULL,
   `image` varchar(255) DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
-  `category` varchar(255) DEFAULT NULL
+  `category` varchar(255) DEFAULT NULL,
+  `seller` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `item`
+--
+
+INSERT INTO `item` (`id`, `name`, `price`, `quantity`, `image`, `description`, `category`, `seller`) VALUES
+(1, 'burger and fries', 12000, 45, 'istockphoto-495204032-612x612.jpg', 'burger and fries', 'fast foods ', 'kfc'),
+(2, 'burger and fries deluxe', 13500, 70, 'istockphoto-1154731746-612x612.jpg', 'food', '', 'kfc'),
+(3, 'hot and spicy wings', 16000, 56, 'KFC-Hot-and-Spicy-Wings-FT-BLOG0923-bcbfe79e54554f249c4bf246d09b1d98.jpg', 'food', 'fast foods ', 'kfc'),
+(4, 'plate of chicken', 6000, 34, 'kfc-chicken.jpg', 'food\r\n', '', 'kfc'),
+(5, 'medium drumsticks', 10000, 56, 'kfc-chicken.jpg', 'food', 'fast foods ', 'kfc'),
+(6, 'springroll bonanza', 5700, 40, '0dc9a70462f8bda4e5d0563ad97fcb66515a44174231ef8e977ecf8fe3d9ec68_uhsjeR.jpg', 'food', 'fast foods ', 'kubo'),
+(7, 'large puff puff', 5000, 200, '360_F_479700407_WHyAvNdeG9yGIjlzHgVclkpGOZ41aevo.jpg', 'food', 'fast foods ', 'kubo'),
+(8, 'plate of springroll', 6000, 45, '360_F_429004893_0Y3UYCrn4mf2iYi4ASKAa10LBvSl32pt.jpg', 'food', 'fast foods ', 'kubo');
 
 -- --------------------------------------------------------
 
@@ -87,6 +102,14 @@ CREATE TABLE `restaurant` (
   `address` varchar(255) DEFAULT NULL,
   `user_type` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `restaurant`
+--
+
+INSERT INTO `restaurant` (`id`, `name`, `phone`, `password`, `email`, `address`, `user_type`) VALUES
+(1, 'kfc', 'vefidi135@gmail.com', '$2y$10$oA38IuAooC6dDJQOsjJKyue0rGzCiB0wUP/YiRIdZXVvZ0fxTnChu', 'vefidi135@gmail.com', NULL, 'restaurant'),
+(2, 'kubo', 'vefidi135@gmail.com', '$2y$10$3O6nYXAPXEwc7/MiP5e/d.fRw1F.Dvc0kPPpE3iOGs.WPQPe3UsnK', 'vefidi135@gmail.com', NULL, 'restaurant');
 
 -- --------------------------------------------------------
 
@@ -182,7 +205,7 @@ ALTER TABLE `cart`
 -- AUTO_INCREMENT for table `item`
 --
 ALTER TABLE `item`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `orders`
@@ -194,7 +217,7 @@ ALTER TABLE `orders`
 -- AUTO_INCREMENT for table `restaurant`
 --
 ALTER TABLE `restaurant`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
