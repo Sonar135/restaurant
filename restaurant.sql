d85ac7f7c897c58339e16ea8a5fa43be314c1588
spark <98943801+Sonar135@users.noreply.github.com>
2024-03-03 21:53:20 +0100

    Add files via upload

--- restaurant.sql
+++ restaurant.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 02, 2024 at 11:03 PM
+-- Generation Time: Mar 03, 2024 at 09:52 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -39,6 +39,16 @@ CREATE TABLE `cart` (
   `food_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`id`, `name`, `price`, `quantity`, `total`, `image`, `seller`, `buyer`, `food_id`) VALUES
+(1, 'hot and spicy wings', 16000, 0, 0, 'KFC-Hot-and-Spicy-Wings-FT-BLOG0923-bcbfe79e54554f249c4bf246d09b1d98.jpg', 'kfc', 'vefidi135@gmail.com', 3),
+(2, 'plate of springroll', 6000, 0, 0, '360_F_429004893_0Y3UYCrn4mf2iYi4ASKAa10LBvSl32pt.jpg', 'kubo', 'vefidi135@gmail.com', 8),
+(3, 'burger and fries deluxe', 13500, 4, 0, 'istockphoto-1154731746-612x612.jpg', 'kfc', 'vefidi135@gmail.com', 2),
+(4, 'large puff puff', 5000, 6, 25000, '360_F_479700407_WHyAvNdeG9yGIjlzHgVclkpGOZ41aevo.jpg', 'kubo', 'vefidi135@gmail.com', 7);
+
 -- --------------------------------------------------------
 
 --
@@ -62,13 +72,13 @@ CREATE TABLE `item` (
 
 INSERT INTO `item` (`id`, `name`, `price`, `quantity`, `image`, `description`, `category`, `seller`) VALUES
 (1, 'burger and fries', 12000, 45, 'istockphoto-495204032-612x612.jpg', 'burger and fries', 'fast foods ', 'kfc'),
-(2, 'burger and fries deluxe', 13500, 70, 'istockphoto-1154731746-612x612.jpg', 'food', '', 'kfc'),
-(3, 'hot and spicy wings', 16000, 56, 'KFC-Hot-and-Spicy-Wings-FT-BLOG0923-bcbfe79e54554f249c4bf246d09b1d98.jpg', 'food', 'fast foods ', 'kfc'),
+(2, 'burger and fries deluxe', 13500, 66, 'istockphoto-1154731746-612x612.jpg', 'food', '', 'kfc'),
+(3, 'hot and spicy wings', 16000, 51, 'KFC-Hot-and-Spicy-Wings-FT-BLOG0923-bcbfe79e54554f249c4bf246d09b1d98.jpg', 'food', 'fast foods ', 'kfc'),
 (4, 'plate of chicken', 6000, 34, 'kfc-chicken.jpg', 'food\r\n', '', 'kfc'),
 (5, 'medium drumsticks', 10000, 56, 'kfc-chicken.jpg', 'food', 'fast foods ', 'kfc'),
 (6, 'springroll bonanza', 5700, 40, '0dc9a70462f8bda4e5d0563ad97fcb66515a44174231ef8e977ecf8fe3d9ec68_uhsjeR.jpg', 'food', 'fast foods ', 'kubo'),
-(7, 'large puff puff', 5000, 200, '360_F_479700407_WHyAvNdeG9yGIjlzHgVclkpGOZ41aevo.jpg', 'food', 'fast foods ', 'kubo'),
-(8, 'plate of springroll', 6000, 45, '360_F_429004893_0Y3UYCrn4mf2iYi4ASKAa10LBvSl32pt.jpg', 'food', 'fast foods ', 'kubo');
+(7, 'large puff puff', 5000, 194, '360_F_479700407_WHyAvNdeG9yGIjlzHgVclkpGOZ41aevo.jpg', 'food', 'fast foods ', 'kubo'),
+(8, 'plate of springroll', 6000, 40, '360_F_429004893_0Y3UYCrn4mf2iYi4ASKAa10LBvSl32pt.jpg', 'food', 'fast foods ', 'kubo');
 
 -- --------------------------------------------------------
 
@@ -152,6 +162,13 @@ CREATE TABLE `wishlist` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `wishlist`
+--
+
+INSERT INTO `wishlist` (`id`, `name`, `price`, `quantity`, `total`, `image`, `buyer`, `food_id`) VALUES
+(1, 'hot and spicy wings', 16000, NULL, NULL, 'KFC-Hot-and-Spicy-Wings-FT-BLOG0923-bcbfe79e54554f249c4bf246d09b1d98.jpg', 'vefidi135@gmail.com', '3');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -199,7 +216,7 @@ ALTER TABLE `wishlist`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `item`
@@ -229,7 +246,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `wishlist`
 --
 ALTER TABLE `wishlist`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
